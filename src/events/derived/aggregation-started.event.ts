99cf9318d5a7ca95f5a29a33c830fc18a5c71f38
thebladehit <boqdanyarmolka@gmail.com>
2026-05-27 17:04:09 +0300

    Add aggregation.started event

---
+++ src/events/derived/aggregation-started.event.ts
@@ -0,0 +1,3 @@
+import type { BaseEvent } from '../base-event';
+
+export type AggregationStartedEvent = BaseEvent;
--- src/events/index.ts
+++ src/events/index.ts
@@ -5,5 +5,6 @@ export type {
   OptimizationInitEvent,
   PromptGeneratedEvent,
   PromptEvaluatedEvent,
+  AggregationStartedEvent,
   OptimizationCompletedEvent,
 } from './derived';
